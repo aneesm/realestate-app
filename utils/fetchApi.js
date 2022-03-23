c5b999590eb18605f28d1d03dfb0ffd7e2078905
mohomedanees <anees@dxdy.tech>
2022-03-23 23:51:40 +0530

    netlify setup

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -2,11 +2,13 @@ import axios from "axios";
 
 export const baseUrl = "https://bayut.p.rapidapi.com";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': ''
+      'x-rapidapi-key': API_KEY
     }
   })
 
